141149583c8f258585563a1c78a60d11e8c48cc4
Sunly <sunly.chao@rexform.io>
2026-03-03 10:41:42 +0700

    GitHub action setup

---
+++ storybook-static/assets/formatter-QJ4M4OGQ-Fyqm0ZIT.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-GHJI37HO-f6FKCNMV.js";import{O as t}from"./iframe-BIXnpt9j.js";import"./preload-helper-PPVm8Dsz.js";import"./react-18-D8e8Tojb.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
